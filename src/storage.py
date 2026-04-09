edad8c4d00940584312ce04fd747bdf717ac42eb
hrithik-sharma25268023 <25268023@studentmail.ul.ie>
2026-04-09 20:59:04 +0100

    read and write PDF

--- src/storage.py
+++ src/storage.py
@@ -51,3 +51,19 @@ class FileSystem:
         """Write bytes to S3 as PNG file"""
 
         self.s3_client.put_object(Bucket=bucket, Key=key, Body=image_bytes, ContentType='image/png')
+
+
+    def read_pdf(self, bucket: str, key: str) -> bytes:
+        """Read PDF file from S3 and return as bytes"""
+
+        response = self.s3_client.get_object(Bucket=bucket, Key=key)
+        return response['Body'].read()
+
+
+    def write_pdf(self, bucket: str, key: str, pdf_bytes: bytes) -> None:
+        """Write bytes to S3 as PDF file"""
+
+        self.s3_client.put_object(Bucket=bucket,Key=key, Body=pdf_bytes, ContentType='application/pdf')
+
+
+
